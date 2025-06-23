03d41e9964029e159eeaf148faedeb70ab5966f9
matiaslarroud <matutedel30@outlook.com>
2025-06-23 15:47:00 -0300

    CORRECCION

--- index.js
+++ index.js
@@ -9,14 +9,15 @@ const app = express();
 mongoose
     .connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinOteca.thjyjhn.mongodb.net/?retryWrites=true&w=majority&appName=Vinoteca`)
     .then((exito) => {console.log("CONEXION EXITOSA A LA BD.")})
-    .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n ERROR: ${error}`)})
+    .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n ERROR: ${error}`)});
 
+    
 app.use(express.json());
     
 app.post('/api/v1/products' , (req , res ) => {
     console.log('Petición recibida.');
     console.log({body: req.body});
-    res.status(201 , {ok : true})
+    res.status(201)
 });
 
 app.use(express.static(path.join(__dirname,'public')));
