e2ae018c0852d248d38c71323c44c9281b350869
matiaslarroud <matutedel30@outlook.com>
2025-06-23 12:50:45 -0300

    CORRECCION

--- index.js
+++ index.js
@@ -8,22 +8,22 @@ require('dotenv').config();
 const app = express();
 
 mongoose
-    .connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinoteca.thjyjhn.mongodb.net/?retryWrites=true&w=majority&appName=Vinoteca`)
+    .connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinOteca.thjyjhn.mongodb.net/?retryWrites=true&w=majority&appName=Vinoteca`)
     .then((exito) => {console.log("CONEXION EXITOSA A LA BD.")})
     .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n ERROR: ${error}`)})
 
-app.use(express.json())
+app.use(express.json());
     
-app.post('/api/v1/products' , (req , res) => {
-    console.log('petición recibida.');
-    console.log({body: req.body})
+app.post('/api/v1/products' , (req , res ) => {
+    console.log('Petición recibida.');
+    console.log({body: req.body});
     res.status(201).json({ok:true})
-})
+});
 
-app.use(express.static(path.join(__dirname,'public')))
+app.use(express.static(path.join(__dirname,'public')));
 
-const PORT = process.env.PORT || 4000
+const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
     console.log("Escuchando puerto ", PORT);
-})
+});
