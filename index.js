d2b0a41c5e740df1092d5e2bdb53bb2725bc11cd
matiaslarroud <matutedel30@outlook.com>
2025-06-23 10:59:23 -0300

    AGREGAMOS FORMULARIO DE PRUEBA

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+require('dotenv').config();
+
 
 const app = express();
 
-mongoose.connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinoteca.thjyjhn.mongodb.net/?retryWrites=true&w=majority&appName=Vinoteca`)
+mongoose
+    .connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinoteca.thjyjhn.mongodb.net/?retryWrites=true&w=majority&appName=Vinoteca`)
     .then((exito) => {console.log("CONEXION EXITOSA A LA BD.")})
-    .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n PASS: ${process.env.PASS_MONGODB} \n ERROR: ${error}`)})
+    .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n ERROR: ${error}`)})
 
 app.get('/' , (req , res , next) => {
     console.log('petición recibida.');
