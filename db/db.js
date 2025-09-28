7feb9be2f0b184bed9e2f027282902b2a8cf9240
matiaslarroud <matutedel30@outlook.com>
2025-09-28 00:49:20 -0300

    CAMBIOS 28-09-2025

--- db/db.js
+++ db/db.js
@@ -1,16 +1,21 @@
-const mongoose = require('mongoose');
-const PORT = process.env.PORT || 4000;
+const mongoose = require("mongoose");
 
 const connectDatabase = (app) => {
-    mongoose
+  const PORT = process.env.PORT || 4000;
+
+  // Levantar el server aunque la BD falle
+  app.listen(PORT, () => {
+    console.log("Servidor escuchando en puerto", PORT);
+  });
+
+  mongoose
     .connect(`mongodb+srv://matutedel30:${process.env.PASS_MONGODB}@vinOteca.thjyjhn.mongodb.net/Vinoteca?retryWrites=true&w=majority&appName=Vinoteca`)
     .then(() => {
-        console.log("CONEXION EXITOSA A LA BD.")
-        app.listen(PORT, () => {
-            console.log("Escuchando puerto ", PORT);
-        });
+      console.log("✅ Conexión exitosa a la BD.");
     })
-    .catch((error) => {console.log(`ERROR DE CONEXION A LA BD. \n ERROR: ${error}`)});
-}
+    .catch((error) => {
+      console.error("❌ Error al conectar a la BD:", error);
+    });
+};
 
 module.exports = connectDatabase;
